5d9bb5855c6e06ef5d5dc9c73e95c978415e402b
David Lefrancq <david.lefrancq@smartness.co>
2022-03-23 11:58:06 +0100

    feat: To Do List

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { CalendarModule } from './calendar/calendar.module';
+import { ToDoListModule } from './to-do-list/to-do-list.module';
 
 @Module({
-  imports: [UserModule, CalendarModule],
+  imports: [UserModule, CalendarModule, ToDoListModule],
   controllers: [AppController],
   providers: [AppService],
 })
