9741a8e007c3fc41fa928c8680dbe3cf996c5fab
David Lefrancq <david.lefrancq@smartness.co>
2022-03-23 12:17:56 +0100

    feat: google signin

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { GoogleSigninModule } from './google-signin/google-signin.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, GoogleSigninModule],
   controllers: [AppController],
   providers: [AppService],
 })
